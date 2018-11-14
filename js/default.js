711d278f2c906d44af74694bf73863c94abd00fd
AlivenYung <michael7351186zx@gmail.com>
2018-11-14 18:00:37 +0800

    Add files via upload

--- js/default.js
+++ js/default.js
@@ -1,16 +1,4 @@
-$(function(){
-	$("#loadBg > #buger").hide();
-	$("#loadBg > #menu").hide();
-	$("#st").hide();
-	$("#loadBg > img").delay(500).animate({right:"10px",width:"50px",height:"50px",top:"48%"},2000);
-	$("#loadBg").delay(2500).animate({width:"70px"},1000);
-	$("#loadBg > img").delay(1000).queue(function (next) { 
-		$(this).css({"animation-play-state":"paused"});
-		$("#loadBg > #buger").show();
-		next();
-	});
 
-});
 function openmenu(){
 	$("#loadBg").css({"width":"300px"});
 	$("#loadBg > #buger").hide();
